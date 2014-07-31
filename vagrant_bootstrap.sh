ea08b6402160fc5dabb3652643304b4a72eb1c20
Chris Charles <chris@ccharles.ca>
2014-07-31 18:24:30 -0400

    Switch from pip to Ubuntu packages for Python libs
    Building from source was taking forever, largely because of
    scipy. Ubuntu packages aren't as cutting edge, but they should do for
    now.

--- vagrant_bootstrap.sh
+++ vagrant_bootstrap.sh
@@ -4,9 +4,13 @@ apt-get install -y build-essential
 apt-get install -y python-dev
 apt-get install -y python-pip
 
-# Required to build scipy from source
-apt-get install -y gfortran
-apt-get install -y libblas-dev
-apt-get install -y liblapack-dev
-
-pip install -r /vagrant/requirements.txt
+# Use Ubuntu's Python packages since some libraries take forever to build
+# (I'm looking at you, scipy).
+apt-get install -y ipython
+apt-get install -y ipython-notebook
+apt-get install -y python-jinja2
+apt-get install -y python-numpy
+apt-get install -y python-pandas
+apt-get install -y python-scipy
+apt-get install -y python-tornado
+apt-get install -y python-zmq
