f21b57a0cfbd7e1aeec5b18fa8bbf98dd8c3b946
Chris Charles <chris@ccharles.ca>
2014-07-31 18:11:45 -0400

    Add scipy

--- vagrant_bootstrap.sh
+++ vagrant_bootstrap.sh
@@ -4,4 +4,9 @@ apt-get install -y build-essential
 apt-get install -y python-dev
 apt-get install -y python-pip
 
+# Required to build scipy from source
+apt-get install -y gfortran
+apt-get install -y libblas-dev
+apt-get install -y liblapack-dev
+
 pip install -r /vagrant/requirements.txt
